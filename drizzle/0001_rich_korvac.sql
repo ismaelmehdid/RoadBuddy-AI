dd5fd10b22a930731b078163d15c8a92d3c229f4
andrii-bluco <andrii@bluco.ai>
2025-05-24 12:33:23 +0200

    DB

---
+++ drizzle/0001_rich_korvac.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "users" ADD COLUMN "firstName" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "lastName" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "name";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "age";
