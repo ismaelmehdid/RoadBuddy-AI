63be81b30ae566611319deea17c85cbf52f1bae2
andrii-bluco <andrii@bluco.ai>
2025-05-24 13:09:35 +0200

    updated DB

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,26 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { ConversationState } from "@/types/types";
+import { integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function enumToPgEnum<T extends Record<string, any>>(
+  myEnum: T,
+): [T[keyof T], ...T[keyof T][]] {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return Object.values(myEnum).map((value: any) => `${value}`) as any;
+}
+
+export const conversationState = pgEnum(
+  'ConversationState',
+  enumToPgEnum(ConversationState),
+);
+
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  firstName: varchar({ length: 255 }).notNull(),
-  lastName: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  phone_number: varchar("phone_number", { length: 15 }).notNull(),
+  conversationState: conversationState('conversationState').default(
+      ConversationState.UNSPECIFIED,
+    ),
+  wrong_answer_count: integer("wrong_answer_count").default(0),
+  correct_answer_count: integer("correct_answer_count").default(0),
+  current_correct_answer_id: varchar("current_correct_answer_id", { length: 1 }).default(''),
 });
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export enum ConversationState {
+  UNSPECIFIED = "UNSPECIFIED",
+  MAIN_MENU = "MAIN_MENU",
+  FLOW = "FLOW",
+  STOPPED = "STOPPED"
+}
